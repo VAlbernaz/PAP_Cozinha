7b7413c15a0fc10f029f18ef51898a7f5f5da55e
Vítor Albernaz <vitor-albernaz@hotmail.com>
2022-02-14 00:15:39 -0100

    14/02

--- src/controller/ControllerCozinha.java
+++ src/controller/ControllerCozinha.java
@@ -1,4 +1,21 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.File;
+
 public class ControllerCozinha {
+
+    @FXML
+    private ImageView IVLogo;
+
+    public void initialize()
+    {
+        //Colocar logotipo
+        File file = new File("logo.png");
+        Image image = new Image(file.toURI().toString());
+        IVLogo.setImage(image);
+    }
 }
